833862e05fb57c8dded69668580a957848221fd0
Jesse Mitchell <TheCreatorJames@live.com>
2017-10-04 00:04:42 -0500

    Preliminary Beautifier

--- src/emulator.controls.js
+++ src/emulator.controls.js
@@ -1,77 +1,81 @@
-
-if ( Emulator === undefined )
+if (Emulator === undefined)
 {
-  throw new Error( "Emulator is not yet defined!" );
+    throw new Error("Emulator is not yet defined!");
 }
 
 Emulator.controls = {
-  running: true,
-  timer: 0,
+    running: true,
+    timer: 0,
 
-  offset: 0
+    offset: 0
 };
 
-Emulator.controls.restart = function() {
-  Emulator.controls.stop();
+Emulator.controls.restart = function()
+{
+    Emulator.controls.stop();
 
-  for ( var i = 0; i < Emulator.registerCount; i++ )
-  {
-    Emulator.setRegister( i, 0 );
-  }
+    for (var i = 0; i < Emulator.registerCount; i++)
+    {
+        Emulator.setRegister(i, 0);
+    }
 
-  Emulator.hideLine();
-  Emulator.controls.offset = 0;
-  Emulator.updateLine();
-  
-  Emulator.controls.running = true;
-  if ( Emulator.controls.timer !== 0 )
-  {
-    Emulator.controls.run()
-  }
-}
-
-Emulator.controls.run = function() {
-  Emulator.controls.timer = setInterval( () => {
-    Emulator.controls.step();
-  }, 500 );
-}
-
-Emulator.controls.stop = function() {
-  clearInterval( Emulator.controls.timer );
-  Emulator.controls.timer = 0;
-}
-
-Emulator.controls.step = function() {
-  if ( !Emulator.controls.running )
-  {
-    return;
-  }
-
-  var instruction = Emulator.address[ Emulator.controls.offset ];
-  if ( instruction.type !== "instruction" )
-  {
-    Emulator.controls.running = false;
-    alert( "Out of instructions: Program Terminated" );
-    return;
-  }
-
-  var fun = Emulator.ops[ instruction.op.val ];
-
-  if ( fun === undefined )
-  {
-    Emulator.controls.running = false;
-    alert( "Undefined opcode: " + instruction.op.val );
-    return;
-  }
-
-  fun( instruction.args );
-
-  if ( !Emulator.controls.running )
-  {
     Emulator.hideLine();
-    return;
-  }
+    Emulator.controls.offset = 0;
+    Emulator.updateLine();
 
-  Emulator.nextLine();
-  Emulator.redraw();
+    Emulator.controls.running = true;
+    if (Emulator.controls.timer !== 0)
+    {
+        Emulator.controls.run()
+    }
+}
+
+Emulator.controls.run = function()
+{
+    Emulator.controls.timer = setInterval(() =>
+    {
+        Emulator.controls.step();
+    }, 500);
+}
+
+Emulator.controls.stop = function()
+{
+    clearInterval(Emulator.controls.timer);
+    Emulator.controls.timer = 0;
+}
+
+Emulator.controls.step = function()
+{
+    if (!Emulator.controls.running)
+    {
+        return;
+    }
+
+    var instruction = Emulator.address[Emulator.controls.offset];
+    if (instruction.type !== "instruction")
+    {
+        Emulator.controls.running = false;
+        alert("Out of instructions: Program Terminated");
+        return;
+    }
+
+    var fun = Emulator.ops[instruction.op.val];
+
+    if (fun === undefined)
+    {
+        Emulator.controls.running = false;
+        alert("Undefined opcode: " + instruction.op.val);
+        return;
+    }
+
+    fun(instruction.args);
+
+    if (!Emulator.controls.running)
+    {
+        Emulator.hideLine();
+        return;
+    }
+
+    Emulator.nextLine();
+    Emulator.redraw();
 };
--- src/emulator.js
+++ src/emulator.js
@@ -1,400 +1,418 @@
-
 /* document set up */
 
 var Emulator = {
 
-  registerCount: 16,
-  registers: [],
+    registerCount: 16,
+    registers: [],
 
-  address: {},
-  labels: {},
+    address:
+    {},
+    labels:
+    {},
 
-  utils: {},
-  ops: {},
-  controls: {},
+    utils:
+    {},
+    ops:
+    {},
+    controls:
+    {},
 
-  registerBase: 10,
-  redraw: function( type ) {
-    if ( type === undefined ) type = Emulator.registerBase;
-    else Emulator.registerBase = type;
-
-    for ( var i = 0; i < Emulator.registerCount; i++ )
+    registerBase: 10,
+    redraw: function(type)
     {
-      var reg = getRegisterObject( i );
-      switch ( type )
-      {
-        case 2:
-          reg = reg.bin();
-          break;
+        if (type === undefined) type = Emulator.registerBase;
+        else Emulator.registerBase = type;
 
-        case 16:
-          reg = reg.hex();
-          break;
+        for (var i = 0; i < Emulator.registerCount; i++)
+        {
+            var reg = getRegisterObject(i);
+            switch (type)
+            {
+                case 2:
+                    reg = reg.bin();
+                    break;
 
-        default:
-          reg = reg.dec();
-          break;
-      }
-      $( "td#r" + i ).text( reg );
-    }
-  },
+                case 16:
+                    reg = reg.hex();
+                    break;
 
-  nextLine: function() {
-    this.hideLine();
-    Emulator.controls.offset += 4;
-    this.updateLine();
-  },
+                default:
+                    reg = reg.dec();
+                    break;
+            }
+            $("td#r" + i).text(reg);
+        }
+    },
 
-  hideLine: function() {
-    $( "li[addr=" + Emulator.controls.offset + "]" ).removeClass( "selected" );
-  },
+    nextLine: function()
+    {
+        this.hideLine();
+        Emulator.controls.offset += 4;
+        this.updateLine();
+    },
 
-  updateLine: function() {
-    $( "li[addr=" + Emulator.controls.offset + "]" ).addClass( "selected" );
-  },
+    hideLine: function()
+    {
+        $("li[addr=" + Emulator.controls.offset + "]").removeClass("selected");
+    },
 
-  ready: function() {
-    
-  },
+    updateLine: function()
+    {
+        $("li[addr=" + Emulator.controls.offset + "]").addClass("selected");
+    },
+
+    ready: function() {
+
+    },
 
 }
 
-Emulator.utils.setStatus = function( bitName, flag )
+Emulator.utils.setStatus = function(bitName, flag)
 {
-  var reg = Emulator.getRegister( 15 );
-  var bitNum;
-  switch ( bitName.toUpperCase() )
-  {
-    case 'N':
-      bitNum = 0;
-      break;
+    var reg = Emulator.getRegister(15);
+    var bitNum;
+    switch (bitName.toUpperCase())
+    {
+        case 'N':
+            bitNum = 0;
+            break;
 
-    case 'Z':
-      bitNum = 1;
-      break;
+        case 'Z':
+            bitNum = 1;
+            break;
 
-    case 'C':
-      bitNum = 2;
-      break;
+        case 'C':
+            bitNum = 2;
+            break;
 
-    case 'V':
-      bitNum = 3;
-      break;
+        case 'V':
+            bitNum = 3;
+            break;
 
-    default:
-      throw new Error( "Waaahhh bitName must be one of: NZCV" );
-  }
+        default:
+            throw new Error("Waaahhh bitName must be one of: NZCV");
+    }
 
-  reg.bits[ bitNum ] = flag;
+    reg.bits[bitNum] = flag;
 }
 
 
 class i32
 {
-  constructor( value )
-  {
-    this.bits = [];
-    for ( var i = 0; i < 32; i++ )
+    constructor(value)
     {
-      this.bits.push( 0 );
-    }
-
-    if ( value === undefined ) return;
-
-    if ( value.constructor === i32 )
-    {
-      this.bits = value.bits;
-    }
-    else if ( value.constructor === Array )
-    {
-      var i = 31;
-      var j = value.length - 1;
-      while ( i >= 0 && j >= 0 )
-      {
-        this.bits[ i ] = parseInt( value[ j ], 2 ) | 0;
-        j--;
-        i--;
-      }
-    }
-    else if ( value < 0 )
-    {
-      this.bits = new i32( -value ).neg().bits;
-    }
-    else
-    {
-      var binary = value.toString( 2 );
-
-      // fill it in, right to left
-      var i = 31;
-      var j = binary.length - 1;
-      while ( i >= 0 && j >= 0 )
-      {
-        this.bits[ i ] = parseInt( binary[ j ], 2 ) | 0
-        j--;
-        i--;
-      }
-    }
-  }
-
-  static fromLiteral( literal )
-  {
-    switch ( literal.subtype )
-    {
-      // the easiest one!
-      case "binary":
-        return new i32( literal.val.split( "" ) );
-
-      // manually expand so we don't lose any precision converting
-      // to floating points
-      case "hexadecimal":
-        var bitpattern = "";
-
-        var split = literal.val.split( "" );
-        for ( var i = 0; i < split.length; i++ )
+        this.bits = [];
+        for (var i = 0; i < 32; i++)
         {
-          var bits = "0000";
-          bits += "0123456789ABCDEF".indexOf( split[ i ].toUpperCase() ).toString( 2 );
-          bitpattern += bits.substr( -4 );
+            this.bits.push(0);
         }
 
-        return new i32( bitpattern );
+        if (value === undefined) return;
 
-      case "decimal":
-        var bitpattern = parseInt( literal.val ).toString( 2 );
-        return new i32( bitpattern );
+        if (value.constructor === i32)
+        {
+            this.bits = value.bits;
+        }
+        else if (value.constructor === Array)
+        {
+            var i = 31;
+            var j = value.length - 1;
+            while (i >= 0 && j >= 0)
+            {
+                this.bits[i] = parseInt(value[j], 2) | 0;
+                j--;
+                i--;
+            }
+        }
+        else if (value < 0)
+        {
+            this.bits = new i32(-value).neg().bits;
+        }
+        else
+        {
+            var binary = value.toString(2);
+
+            // fill it in, right to left
+            var i = 31;
+            var j = binary.length - 1;
+            while (i >= 0 && j >= 0)
+            {
+                this.bits[i] = parseInt(binary[j], 2) | 0
+                j--;
+                i--;
+            }
+        }
     }
-  }
 
-  copy( other )
-  {
-    this.bits = other.bits;
-  }
-
-  flip()
-  {
-    for ( var i = 31; i >= 0; i-- )
+    static fromLiteral(literal)
     {
-      this.bits[ i ] ^= 1;
+        switch (literal.subtype)
+        {
+            // the easiest one!
+            case "binary":
+                return new i32(literal.val.split(""));
+
+                // manually expand so we don't lose any precision converting
+                // to floating points
+            case "hexadecimal":
+                var bitpattern = "";
+
+                var split = literal.val.split("");
+                for (var i = 0; i < split.length; i++)
+                {
+                    var bits = "0000";
+                    bits += "0123456789ABCDEF".indexOf(split[i].toUpperCase()).toString(2);
+                    bitpattern += bits.substr(-4);
+                }
+
+                return new i32(bitpattern);
+
+            case "decimal":
+                var bitpattern = parseInt(literal.val).toString(2);
+                return new i32(bitpattern);
+        }
     }
 
-    return this;
-  }
-
-  neg()
-  {
-    if ( this.bits[ 0 ] === 0 )
-      return this.flip().add( new i32( 1 ), false );
-    else
-      return this.sub( new i32( 1 ), false ).flip();
-  }
-
-  sub( other, affectStatus )
-  {
-    // haha, just think about how this works if `other` is negative
-    return this.add( other.neg(), affectStatus );
-  }
-
-  add( other, affectStatus )
-  {
-    if ( affectStatus === undefined ) affectStatus = false;
-
-    var result = new i32(); 
-    var carry = [ 0 ];
-    for ( var i = 31; i >= 0; i-- )
+    copy(other)
     {
-      var sum = this.bits[ i ] + other.bits[ i ] + carry[ 0 ];
-
-      if ( sum === 0 || sum === 1 )
-      {
-        result.bits[ i ] = sum;
-        carry.unshift( 0 );
-      }
-      else if ( sum === 2 || sum === 3 )
-      {
-        result.bits[ i ] = sum - 2;
-        carry.unshift( 1 );
-      }
+        this.bits = other.bits;
     }
 
-    if ( affectStatus )
+    flip()
     {
-      Emulator.utils.setStatus( 'N', result.bits[ 0 ] ); // 0 => positive, 1 => negative
-      Emulator.utils.setStatus( 'Z', result.zero() ? 1 : 0 );
-      Emulator.utils.setStatus( 'C',   carry !== 0 ? 1 : 0 );
-      Emulator.utils.setStatus( 'V', carry[ 0 ] ^ carry[ 1 ] ) // I got this from some university page
+        for (var i = 31; i >= 0; i--)
+        {
+            this.bits[i] ^= 1;
+        }
+
+        return this;
     }
 
-    return result;
-  }
-
-  zero()
-  {
-    return this.dec() === 0;
-  }
-
-  bin()
-  {
-    return "0b" + this.bits.join( "" );
-  }
-
-  hex()
-  {
-    return "0x" + ( "00000000" + parseInt( this.bin().substring( 2 ), 2 ).toString( 16 ).toUpperCase() ).substr( -8 );
-  }
-
-  dec( signed )
-  {
-    if ( signed )
+    neg()
     {
-      if ( this.bits[ 0 ] === 0 )
-      {
-        return this.dec( false );
-      }
-      else
-      {
-        return -( this.neg().dec( true ) ); 
-      }
+        if (this.bits[0] === 0)
+            return this.flip().add(new i32(1), false);
+        else
+            return this.sub(new i32(1), false).flip();
     }
-    else
+
+    sub(other, affectStatus)
     {
-      return parseInt( this.bin().substring( 2 ), 2 );
+        // haha, just think about how this works if `other` is negative
+        return this.add(other.neg(), affectStatus);
     }
-  }
+
+    add(other, affectStatus)
+    {
+        if (affectStatus === undefined) affectStatus = false;
+
+        var result = new i32();
+        var carry = [0];
+        for (var i = 31; i >= 0; i--)
+        {
+            var sum = this.bits[i] + other.bits[i] + carry[0];
+
+            if (sum === 0 || sum === 1)
+            {
+                result.bits[i] = sum;
+                carry.unshift(0);
+            }
+            else if (sum === 2 || sum === 3)
+            {
+                result.bits[i] = sum - 2;
+                carry.unshift(1);
+            }
+        }
+
+        if (affectStatus)
+        {
+            Emulator.utils.setStatus('N', result.bits[0]); // 0 => positive, 1 => negative
+            Emulator.utils.setStatus('Z', result.zero() ? 1 : 0);
+            Emulator.utils.setStatus('C', carry !== 0 ? 1 : 0);
+            Emulator.utils.setStatus('V', carry[0] ^ carry[1]) // I got this from some university page
+        }
+
+        return result;
+    }
+
+    zero()
+    {
+        return this.dec() === 0;
+    }
+
+    bin()
+    {
+        return "0b" + this.bits.join("");
+    }
+
+    hex()
+    {
+        return "0x" + ("00000000" + parseInt(this.bin().substring(2), 2).toString(16).toUpperCase()).substr(-8);
+    }
+
+    dec(signed)
+    {
+        if (signed)
+        {
+            if (this.bits[0] === 0)
+            {
+                return this.dec(false);
+            }
+            else
+            {
+                return -(this.neg().dec(true));
+            }
+        }
+        else
+        {
+            return parseInt(this.bin().substring(2), 2);
+        }
+    }
 }
 
-$( document ).ready( function() {
-  $( "div#source" ).css( "margin-left", $( "div#tools" ).outerWidth() + 10 );
-} );
+$(document).ready(function()
+{
+    $("div#source").css("margin-left", $("div#tools").outerWidth() + 10);
+});
 
-$( "div#source ul li" ).first().each( function() {
+$("div#source ul li").first().each(function()
+{
 
-  $( this ).addClass( "selected" );
+    $(this).addClass("selected");
 
-} );
+});
 
 Emulator.ready = function()
 {
-  $( "pre#src" ).remove();
-  var tools = $( "<div></div>" ).attr( "id", "tools" ).appendTo( "body" );
+    $("pre#src").remove();
+    var tools = $("<div></div>").attr("id", "tools").appendTo("body");
 
-  // add controls
-  $( "<div></div>" ).attr( "id", "controls" ).appendTo( tools );
-  $( "div#controls" ).each( function() {
+    // add controls
+    $("<div></div>").attr("id", "controls").appendTo(tools);
+    $("div#controls").each(function()
+    {
 
-    var self = $( this );
-    var buttons = $( "<div></div>" ).attr( "id", "controlButtons" ).appendTo( self );
+        var self = $(this);
+        var buttons = $("<div></div>").attr("id", "controlButtons").appendTo(self);
 
-    $( "<button></button>" ).html( "Run" ).click( function() {
-      
-      var self = $( this );
-      if ( self.html() == "Run" )
-      {
-        self.html( "Stop" );
-        Emulator.controls.run();
-      }
-      else
-      {
-        self.html( "Run" );
-        Emulator.controls.stop();
-      }
-
-    } ).appendTo( buttons );
-    $( "<button></button>" ).html( "Step Once" ).attr( "onclick", "Emulator.controls.step()" ).appendTo( buttons );
-    $( "<button></button>" ).html( "Restart" ).attr( "onclick", "Emulator.controls.restart()" ).appendTo( buttons );
-
-    var display = $( "<div></div>" ).attr( "id", "baseDisplay" ).appendTo( self );
-    function radioButton( text, base ) {
-      var label = $( "<label>" ).text( text );
-      var input = $( "<input type='radio' name='registerBase'>" );
-
-      input.click( function() {
-        if ( $( this ).is( ":checked" ) )
+        $("<button></button>").html("Run").click(function()
         {
-          Emulator.redraw( base );
+
+            var self = $(this);
+            if (self.html() == "Run")
+            {
+                self.html("Stop");
+                Emulator.controls.run();
+            }
+            else
+            {
+                self.html("Run");
+                Emulator.controls.stop();
+            }
+
+        }).appendTo(buttons);
+        $("<button></button>").html("Step Once").attr("onclick", "Emulator.controls.step()").appendTo(buttons);
+        $("<button></button>").html("Restart").attr("onclick", "Emulator.controls.restart()").appendTo(buttons);
+
+        var display = $("<div></div>").attr("id", "baseDisplay").appendTo(self);
+
+        function radioButton(text, base)
+        {
+            var label = $("<label>").text(text);
+            var input = $("<input type='radio' name='registerBase'>");
+
+            input.click(function()
+            {
+                if ($(this).is(":checked"))
+                {
+                    Emulator.redraw(base);
+                }
+            });
+
+            display.append(input);
+            display.append(label).append("<br/>");
         }
-      } );
 
-      display.append( input );
-      display.append( label ).append( "<br/>" );
-    }
+        radioButton("Hexadecimal", 16);
+        radioButton("Decimal", 10);
+        radioButton("Binary", 2);
 
-    radioButton( "Hexadecimal", 16 );
-    radioButton( "Decimal", 10 );
-    radioButton( "Binary", 2 );
+    });
 
-  } );
-
-  // add registers
-  $( "<div></div>" ).attr( "id", "registers" ).appendTo( tools );
-  $( "div#registers" ).each( function() {
-
-    var self = $(this);
-    var table = $( "<table></table>" ).appendTo( self );
-
-    for ( var i = 0; i < Emulator.registerCount; i++ )
+    // add registers
+    $("<div></div>").attr("id", "registers").appendTo(tools);
+    $("div#registers").each(function()
     {
-      var name = "r" + i;
-      
-      var header = $( "<th></th>" ).html( name );
-      var value = $( "<td></td>" ).html( "0" ).attr( "id", name );
 
-      Emulator.registers[ i ] = new i32();
+        var self = $(this);
+        var table = $("<table></table>").appendTo(self);
 
-      $( "<tr></tr>" ).append( header ).append( value ).appendTo( table );
-    }
+        for (var i = 0; i < Emulator.registerCount; i++)
+        {
+            var name = "r" + i;
 
-    var test = "0b";
-    for ( var i = 0; i < 32; i++ )
-    {
-      test += "0";
-    }
-    var row = $( "<tr></tr>" );
-    row.append( "<td></td>" );
-    $( "<td></td>" ).html( test ).css( "visibility", "hidden" ).appendTo( row );
-    row.appendTo( table );
-  } );
+            var header = $("<th></th>").html(name);
+            var value = $("<td></td>").html("0").attr("id", name);
 
-  // process the parsed source back into a displayable format
-  var source = $( "<div></div>" ).attr( "id", "source" ).appendTo( "body" );
-  var list = $( "<ul></ul>" ).appendTo( source );
+            Emulator.registers[i] = new i32();
+
+            $("<tr></tr>").append(header).append(value).appendTo(table);
+        }
+
+        var test = "0b";
+        for (var i = 0; i < 32; i++)
+        {
+            test += "0";
+        }
+        var row = $("<tr></tr>");
+        row.append("<td></td>");
+        $("<td></td>").html(test).css("visibility", "hidden").appendTo(row);
+        row.appendTo(table);
+    });
+
+    // process the parsed source back into a displayable format
+    var source = $("<div></div>").attr("id", "source").appendTo("body");
+    var list = $("<ul></ul>").appendTo(source);
 
 }
 
 /* emulation */
 
-function getRegisterObject( i )
+function getRegisterObject(i)
 {
-  if ( typeof( i ) === "string" ) 
-  {
-    return Emulator.registers[ parseInt( i.substring( 1 ) ) ];
-  }
-  else
-  {
-    return Emulator.registers[ i ];
-  }
+    if (typeof(i) === "string")
+    {
+        return Emulator.registers[parseInt(i.substring(1))];
+    }
+    else
+    {
+        return Emulator.registers[i];
+    }
 }
 
-Emulator.setRegister = function( i, val ) {
-  if ( val.constructor === i32 )
-  {
-    getRegisterObject( i ).copy( val );
-  }
-  else if ( val.type !== undefined && val.type === "literal" )
-  {
-    getRegisterObject( i ).copy( i32.fromLiteral( val ) );
-  }
-  else if ( typeof( val ) === "number" )
-  {
-    getRegisterObject( i ).copy( new i32( val ) );
-  }
-  Emulator.redraw();
+Emulator.setRegister = function(i, val)
+{
+    if (val.constructor === i32)
+    {
+        getRegisterObject(i).copy(val);
+    }
+    else if (val.type !== undefined && val.type === "literal")
+    {
+        getRegisterObject(i).copy(i32.fromLiteral(val));
+    }
+    else if (typeof(val) === "number")
+    {
+        getRegisterObject(i).copy(new i32(val));
+    }
+    Emulator.redraw();
 };
 
-Emulator.getRegister = function( i ) {
-  return getRegisterObject( i );
+Emulator.getRegister = function(i)
+{
+    return getRegisterObject(i);
 };
 
-function isRegister( val ) {
-  return val.toLowerCase().startsWith( "r" );
+function isRegister(val)
+{
+    return val.toLowerCase().startsWith("r");
 }
-
